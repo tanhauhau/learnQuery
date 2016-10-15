ec5816ebaec8888ade83dd8a24f68fe88ae5f260
Tan Li Hau <lhtan93@gmail.com>
2016-10-15 17:04:58 +0800

    04

--- 04.DOM_manipulation/src/domManipulation.js
+++ 04.DOM_manipulation/src/domManipulation.js
@@ -1,3 +1,44 @@
-var dom = function(){
-  // code goes here
-};
+var dom = (function(){
+  return {
+    remove: remove,
+    append: append,
+    prepend: prepend,
+    after: after,
+    before: before,
+    val: val,
+  };
+
+  function remove(element){
+    if(element.parentNode){
+      element.parentNode.removeChild(element);
+    }
+  }
+  function append(element, targetElem){
+    element.appendChild(targetElem);
+  }
+  function prepend(element, targetElem){
+    var child = element.firstChild;
+    if(child){
+      element.insertBefore(targetElem, child);
+    }else{
+      append(element, targetElem);
+    }
+  }
+  function after(element, targetElem){
+    if(element.parentNode){
+      if(element.nextSibling){
+        element.parentNode.insertBefore(targetElem, element.nextSibling);
+      }else{
+        append(element.parentNode, targetElem);
+      }
+    }
+  }
+  function before(element, targetElem){
+    if(element.parentNode){
+      element.parentNode.insertBefore(targetElem, element);
+    }
+  }
+  function val(element){
+    return element.value || element.getAttributeNode('value').nodeValue;
+  }
+})();
