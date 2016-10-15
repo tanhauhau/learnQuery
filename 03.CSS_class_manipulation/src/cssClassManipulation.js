6f2ba4b050fb8578bc5fb32182e3def98b7d7e79
Tan Li Hau <lhtan93@gmail.com>
2016-10-15 17:04:52 +0800

    03

--- 03.CSS_class_manipulation/src/cssClassManipulation.js
+++ 03.CSS_class_manipulation/src/cssClassManipulation.js
@@ -1,5 +1,23 @@
 var cssClass = (function() {
   'use strict';
 
-  // code goes here
+  return {
+    add: add,
+    remove: remove,
+    toggle: toggle,
+    has: has
+  };
+
+  function add(element, clz){
+    return element.classList.add(clz);
+  }
+  function remove(element, clz){
+    return element.classList.remove(clz);
+  }
+  function toggle(element, clz){
+    return element.classList.toggle(clz);
+  }
+  function has(element, clz){
+    return element.classList.contains(clz);
+  }
 })();
